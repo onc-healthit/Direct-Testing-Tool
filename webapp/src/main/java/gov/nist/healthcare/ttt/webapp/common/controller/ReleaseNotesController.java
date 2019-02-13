a33370675cc80f0e03d80563af61ad412e804150
bgirishrao <girishrao.b@gmail.com>
2019-02-13 00:22:44 -0500

    Fixed to read file from local

--- webapp/src/main/java/gov/nist/healthcare/ttt/webapp/common/controller/ReleaseNotesController.java
+++ webapp/src/main/java/gov/nist/healthcare/ttt/webapp/common/controller/ReleaseNotesController.java
@@ -1,5 +1,6 @@
 package gov.nist.healthcare.ttt.webapp.common.controller;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,10 +10,10 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,7 +33,7 @@ public class ReleaseNotesController {
 		try {
 			resource = appContext.getResource("file:" + System.getProperty("user.dir")+System.getProperty("file.separator")+"release_notes.txt");
 			if (resource.exists()){
-				return FileUtils.readFileToString(resource.getFile());				
+				return FileUtils.readFileToString(new File(resource.getFilename()));			
 			}else{
 				InputStream in = new URL(releaseNotesPath).openStream();
 				return IOUtils.toString(in);				
